e4fffac16ce38071b504f3868e6602a97fb84ae0
Omkar Mhaske <mhaskeomkar322@gmail.com>
2025-08-03 13:44:27 +0530

    program completed

--- calculator.py
+++ calculator.py
@@ -1,2 +1,17 @@
 # Python Programming 
 # Task 1: Create a Simple Calculator
+
+operations = input("Enter an operator (+, -, *, /): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if operations == '+':
+    print(num1 + num2)
+elif operations == '-':
+    print(num1 - num2)
+elif operations == '*':
+    print(num1 * num2)
+elif operations == '/':
+    print(num1 / num2)
+else:
+    print(f"{operations} is not a valid operator.")
